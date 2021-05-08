03631823b4302d49aa8fdd2577c35f5e16921821
ALowton <ALowton@QA.com>
2021-05-08 19:11:17 +0100

    AL: post request to API

--- src/main/resources/static/js/hwa.js
+++ src/main/resources/static/js/hwa.js
@@ -1,18 +1,22 @@
 "use strict";
 
+// Host:Port
 const ADDR = "http://localhost:8080";
+
+// Divs
 const RESULTS_DIV = document.querySelector("#results-div");
 const FORM_DIV = document.querySelector("#form-div");
+
+// Buttons
 const CREATE_BTN = document.querySelector("#create_btn");
 
-// Variables used for testing post
+// Inputs
 const NAME = document.querySelector(".name-input");
-const JOB = document.querySelector(".artist-input");
-// const ALERT_SUC = document.querySelector("#onSuccess");
-// const ALERT_ERR = document.querySelector("#onError");
+const ARTIST = document.querySelector(".artist-input");
+const YEAR = document.querySelector(".year-input");
+const TYPE = document.querySelector(".type-input");
 
-
-
+// Get request
 const getAll = () => {
     axios.get(`${ADDR}/getAll`)
         .then((resp) => {
@@ -24,45 +28,61 @@ const getAll = () => {
         }).catch((err) => console.error(err))
 }
 
+// Post request
+const create = () => {
+    // Input values
+    const NAME_VALUE = NAME.value;
+    const ARTIST_VALUE = ARTIST.value;
+    const YEAR_VALUE = YEAR.value;
+    const TYPE_VALUE = TYPE.value;
+
+    // For checking input vals
+    // console.log(`${NAME_VALUE}${ARTIST_VALUE}${YEAR_VALUE}${TYPE_VALUE}`);
+
+    // New object to be created
+    let obj = {
+        "name": NAME_VALUE,
+        "artist": ARTIST_VALUE,
+        "year": YEAR_VALUE,
+        "type": TYPE_VALUE
+    };
+
+    axios.post(`${ADDR}/create`, obj)
+        .then((resp) => {
+            statusMsg(true);
+            getAll();
+        }).catch((err) => {
+            console.error(err);
+            statusMsg(false);
+        })
+}
+
+const statusMsg = (bool) => {
+    CREATE_BTN.setAttribute("disabled", "true");
+    if (bool) {
+        const ALERT_SUC = document.createElement("div");
+        ALERT_SUC.setAttribute("class", "alert alert-success");
+        ALERT_SUC.innerHTML = "Creation Successful"
+        FORM_DIV.appendChild(ALERT_SUC);
+        setTimeout(() => {
+            FORM_DIV.removeChild(document.querySelector(".alert-success"));
+            CREATE_BTN.removeAttribute("disabled");
+        }, 3000)
+    } else {
+        const ALERT_ERR = document.createElement("div");
+        ALERT_ERR.setAttribute("class", "alert alert-danger");
+        ALERT_ERR.innerHTML = "Creation Failed";
+        FORM_DIV.appendChild(ALERT_ERR);
+        setTimeout(() => {
+            FORM_DIV.removeChild(document.querySelector(".alert-danger"));
+            CREATE_BTN.removeAttribute("disabled");
+        }, 3000);
+    }
+}
+
 const printResult = (result) => {
     const P = document.createElement("p");
     const TEXT = document.createTextNode(`Name: ${result.name} Artist: ${result.artist} Year: ${result.year} Type: ${result.type}`);
     P.appendChild(TEXT);
     RESULTS_DIV.appendChild(P);
-}
-
-// Function used for testing post
-const postTest = () => {
-    const NAME_VALUE = NAME.value;
-    const JOB_VALUE = JOB.value;
-
-    console.log(`${NAME_VALUE}${JOB_VALUE}`);
-
-    let obj = {
-        "name": NAME_VALUE,
-        "job": JOB_VALUE
-    };
-
-    axios.post("https://reqres.in/api/users", obj)
-        .then((resp) => {
-            CREATE_BTN.setAttribute("disabled", "true");
-            const ALERT_SUC = document.createElement("div");
-            ALERT_SUC.setAttribute("class", "alert alert-success");
-            ALERT_SUC.innerHTML = "Creation Successful"
-            FORM_DIV.appendChild(ALERT_SUC);
-            setTimeout(() => {
-                FORM_DIV.removeChild(document.querySelector(".alert-success"));
-                CREATE_BTN.removeAttribute("disabled");
-            }, 3000)
-            getAll();
-        }).catch((err) => {
-            console.error(err);
-            const ALERT_ERR = document.createElement("div");
-            ALERT_ERR.setAttribute("class", "alert alert-danger");
-            ALERT_ERR.innerHTML = "Creation Failed";
-            FORM_DIV.appendChild(ALERT_ERR);
-            setTimeout(() => {
-                FORM_DIV.removeChild(document.querySelector(".alert-danger"));
-            }, 3000);
-        })
 }
